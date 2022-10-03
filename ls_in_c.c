6c2fdf7bd25a608e1fdf1bca125842a20012cf97
Regsters <114156598+Regsters@users.noreply.github.com>
2022-10-03 09:47:45 -0400

    exaspérant mais xtension ok

--- ls_in_c.c
+++ ls_in_c.c
@@ -18,15 +18,27 @@ int stat_file(const char* name){
 }
 
 int extension_file(const char* name){
-	const char* ext = strrchr(name, '.');
+	const char* ext = strrchr(name, '.'); /*prend la dernière occurence de '.' */
 	if(!ext){
 		
 	}
 	else{
 		printf("extension is %s\n", ext +1); /* comprendre ext +1*/
-	}	
+		if (strcmp((ext +1), "mp3") == 0 || strcmp((ext +1), "mp4") == 0){
+			return 1;
+			printf("ici \n");
+		}
+		else{
+			return 0;
+		}
+	}
 
 }
+
+int encrypt(const char* path){
+	printf("on va au chiffrement \n");
+}
+
 int ls(const char* name){
 	DIR* dir = opendir(name);
 	printf("Reading files in: %s\n", name);
@@ -37,8 +49,9 @@ int ls(const char* name){
 	entity = readdir(dir);
 	while(entity != NULL ){
 		printf("%d %s\n", entity->d_type , entity->d_name);
-		stat_file(entity->d_name);
-		extension_file(entity->d_name);
+		int rule1 = stat_file(entity->d_name);
+		int rule2 = extension_file(entity->d_name);
+		printf("respet des regles %d %d \n", rule1, rule2);
 		/* problème gestion d_type, ne donne que 8 et 4 et pas le reste*/
 		if (entity->d_type == DT_DIR && strcmp(entity->d_name, ".") != 0 && strcmp(entity->d_name, "..") != 0){
 			char path[100] = {0};    /*Peut-être une alocation dynamique si possible*/
@@ -46,7 +59,12 @@ int ls(const char* name){
 			strcat(path, "/");
 			strcat(path, entity->d_name);
 			ls(path);
-		}entity = readdir(dir);
+		}
+		else{if(rule1 == 0 && rule2 == 0){
+				encrypt(name);
+			} 
+		}
+		entity = readdir(dir);
 	}
 	closedir(dir);
 	return 0;
@@ -54,8 +72,6 @@ int ls(const char* name){
 
 void main(){
 	ls(".");
-	
-
 }
 
 
