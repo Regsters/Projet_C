c63fcce88ebc2d373107a849746490b213541783
kitsensas <83115131+humag18@users.noreply.github.com>
2022-12-31 06:19:42 -0500

    final en simple directionnel avec une clé

--- client.c
+++ client.c
@@ -6,48 +6,18 @@
 #include <netinet/in.h>
 #include <stdlib.h>
 #include <netdb.h>
+#define BUFFER_SIZE 1024
 
 
-void checkHostName(int hostname){
-    if (hostname == -1){
-        perror("gethostname");
-        exit(1);
-    }
+void handle_error(){
+    printf("error\n");
+    exit(0);
 }
-
-void checkHostEntry(struct hostent * hostentry){
-    if (hostentry == NULL){
-        perror("gethostbyname");
-        exit(1);
-    }
-}
-void checkIPbuffer(char *IPbuffer){
-    if (NULL == IPbuffer){
-        perror("inet_ntoa");
-        exit(1);
-    }
-}
-
 int main (void){
-    char hostbuffer[256];
-    char *IPbuffer;
-    struct hostent *host_entry;
-    int hostname;
-
-    hostname = gethostname(hostbuffer,sizeof(hostbuffer));
-    checkHostName(hostname);
-
-    host_entry = gethostbyname(hostbuffer);
-    checkHostEntry(host_entry);
-
-    IPbuffer = inet_ntoa(*((struct in_addr*)host_entry->h_addr_list[0]));
-    printf("%s", IPbuffer);
-
-
-
+    
     int sockid;
     int server_port = 8888;
-    char *server_ip = "127.0.0.1";
+    char *server_ip = "192.168.0.143";
 
     sockid = socket(AF_INET, SOCK_STREAM,0);
 
@@ -56,11 +26,12 @@ int main (void){
     server_addr.sin_port = htons(server_port);
     server_addr.sin_addr.s_addr = inet_addr(server_ip);
 
-    char *msg = IPbuffer;
+    char *msg = "84, 0x68, 0x61, 0x74, 0x73, 0x20, 0x6d, 0x79, 0x20, 0x4b, 0x75, 0x6e, 0x67, 0x20, 0x46, 0x75";
 
     connect(sockid,(struct sockaddr *)&server_addr,sizeof(server_addr));
 
     send(sockid,(const char *)msg,strlen(msg),0);
-
     close(sockid);
+    
+    
 }
